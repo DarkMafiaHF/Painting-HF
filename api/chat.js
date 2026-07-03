56fbc77194019beb5d195f4975c4920eff3daaf6
DarkMafia <r4rhawa@gmail.com>
2026-07-03 19:20:47 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -7,7 +7,7 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://adv-pristine-estimatescheduler.web.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://advpristinepainting.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
